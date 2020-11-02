3dafacf4a30d5a50dd788dded5269c40d3920d92
wricky <rahulrajan5410@gmail.com>
2020-11-02 17:05:03 +0530

    ADDED: employe function To get daily and total wage added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,11 @@
-"welcome"
+totalSalary=$(($totalEmpHrs+$EMP_RATE_PER_HR));
+echo ${dailyWage[@]}
+echo ${!dailyWage[@]}
+
+echo "Day no.           DailyWage               TotalWage"
+for((i=1;i<day;i++))
+do
+        echo "day$i"    ${dailywage[Day $i]}      ${totalWage[Day $i]}"
+done
+
 
