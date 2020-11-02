bbae1effb76d7dd75fa55fbf0b4c3875a7f32fdd
wricky <rahulrajan5410@gmail.com>
2020-11-02 15:39:34 +0530

    ADDED: employe attendence functionality added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,13 @@
-"welcome"
+attend=$((RANDOM%2))
+full_wage=160
+no_wage=0
+
+if (( $attend == 0 ))
+then
+        echo "Day No:" "Wage"
+        echo "Day 1  "  $full_wage
+else
+        echo "Day No:" "Wage"
+        echo "Day 1  "  $no_wage
+fi
 
