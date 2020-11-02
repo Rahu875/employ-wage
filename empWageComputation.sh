c2766c024ca388450cec66cb00f6e704e03c60f6
wricky <rahulrajan5410@gmail.com>
2020-11-02 16:50:31 +0530

    ADDED: employe totalwrkinghours in month added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,21 @@
+
+MAX_HRS_IN_MONTH=8;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+        $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3));
 dailyWageempHrs="$( getWorkHrs $empCheck )"
 totalEmpHrs=$(($totalEmpHrs+$empHrs))
 dailyWage["Day"$totalWorkingDays]="$( getEmpWage $empHrs )"
+done
+totalSalary=$(($totalEmpHrs+$EMP_RATE_PER_HR));
+echo ${dailyWage[@]}
+echo ${!dailyWage[@]}
+
+
+
 
 
