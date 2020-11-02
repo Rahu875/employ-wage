aa9cd7386e3f638cfe27fb9ed3f4598651a656d6
wricky <rahulrajan5410@gmail.com>
2020-11-02 16:19:43 +0530

    ADDED: employe partimeEmployeWage functionality added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,11 @@
-"welcome"
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+        $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3));
+empHrs="$( getWorkHrs $empCheck )"
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+dailyWage["Day"$totalWorkingDays]="$( getEmpWage $empHrs )"
+done
+
 
