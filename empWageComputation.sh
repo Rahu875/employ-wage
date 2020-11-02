6d00b98dcbcb09940d8c03b7dc58fd6862e5d201
wricky <rahulrajan5410@gmail.com>
2020-11-02 17:12:41 +0530

    ADDED: employe total wage added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,19 @@
-"welcome"
+IS_PART_TIME=1;
+IS_Full_TIME=2;
+MAX_HRS_IN_MONTH=8;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+declare -A dailyWage;
+dailyWageempHrs="$( getWorkHrs $empCheck )"
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+dailyWage["Day"$totalWorkingDays]="$( getEmpWage $empHrs )"
+for((i=1;i<day;i++))
+do
+        echo "day$i"    ${dailywage[Day $i]}      ${totalWage[Day $i]}"
+done
+
+
 
